e9fe1780c725ac683d12d3023ff3469d873892c7
Aanand-Patil <anandpatil6440@gmail.com>
2025-05-21 13:08:56 +0530

    layout updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,25 @@
-import "./globals.css";
+import LeftBar from '@/components/LeftBar'
+import './globals.css'
+import RightBar from '@/components/RightBar'
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang='en'>
+      <body>
+        <div className=''>
+          <div className=''>
+            <LeftBar />
+          </div>
+          <div className=''>{children}</div>
+          <div className=''>
+            <RightBar />
+          </div>
+        </div>
+      </body>
     </html>
-  );
+  )
 }
